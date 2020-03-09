b0200bb6fce472682050210c79b64ad8a5d92086
James Harris <mailjamesharris@gmail.com>
2020-03-10 08:52:56 +1000

    Add envelope fixture.

---
+++ fixtures/doc.go
@@ -0,0 +1,2 @@
+// Package fixtures is a set of test fixtures and mocks of the various types.
+package fixtures
---
+++ fixtures/envelope.go
@@ -0,0 +1,75 @@
+package fixtures
+
+import (
+	"time"
+
+	"github.com/dogmatiq/configkit"
+	"github.com/dogmatiq/dogma"
+	"github.com/dogmatiq/infix/envelope"
+	"github.com/dogmatiq/marshalkit"
+	"github.com/dogmatiq/marshalkit/fixtures"
+)
+
+// NewEnvelope returns a new envelope containing the given message.
+//
+// times can contain up to two elements, the first is the created time, the
+// second is the scheduled-for time.
+func NewEnvelope(
+	id string,
+	m dogma.Message,
+	times ...time.Time,
+) *envelope.Envelope {
+	env := &envelope.Envelope{
+		MetaData: envelope.MetaData{
+			MessageID:     id,
+			CausationID:   "<cause>",
+			CorrelationID: "<correlation>",
+			Source: envelope.Source{
+				Application: configkit.MustNewIdentity("<app-name>", "<app-key>"),
+				Handler:     configkit.MustNewIdentity("<handler-name>", "<handler-key>"),
+				InstanceID:  "<instance>",
+			},
+		},
+		Message: m,
+		Packet: marshalkit.MustMarshalMessage(
+			fixtures.Marshaler,
+			m,
+		),
+	}
+
+	switch len(times) {
+	case 0:
+		env.MetaData.CreatedAt = time.Now()
+	case 1:
+		env.MetaData.CreatedAt = times[0]
+	case 2:
+		env.MetaData.CreatedAt = times[0]
+		env.MetaData.ScheduledFor = times[1]
+	default:
+		panic("too many times specified")
+	}
+
+	cleanseTime(&env.MetaData.CreatedAt)
+	cleanseTime(&env.MetaData.ScheduledFor)
+
+	return env
+}
+
+// cleanseTime marshals/unmarshals time to strip any internal state that would
+// not be transmitted across the network.
+func cleanseTime(t *time.Time) {
+	if t.IsZero() {
+		*t = time.Time{}
+		return
+	}
+
+	data, err := t.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.UnmarshalBinary(data)
+	if err != nil {
+		panic(err)
+	}
+}
