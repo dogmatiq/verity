127dd2406e09c3897a1440796f7dbafb222a30da
James Harris <mailjamesharris@gmail.com>
2020-03-12 15:27:21 +1000

    Add a `StreamDriver` abstraction and refactor PostgreSQL stream to use.

---
+++ persistence/sql/driver/stream.go
@@ -0,0 +1,64 @@
+package driver
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/dogmatiq/infix/persistence"
+
+	"github.com/dogmatiq/infix/envelope"
+)
+
+// StreamDriver is an interface used by sql.Stream to interface with the
+// underlying database.
+type StreamDriver interface {
+	// FindFilter finds a filter by its hash and type names.
+	FindFilter(
+		ctx context.Context,
+		db *sql.DB,
+		hash []byte,
+		names []string,
+	) (uint64, bool, error)
+
+	// CreateFilter creates a filter with the specified hash and type names.
+	CreateFilter(
+		ctx context.Context,
+		db *sql.DB,
+		hash []byte,
+		names []string,
+	) (uint64, error)
+
+	// IncrementOffset increments an application stream's next offset by the
+	// specified amount and returns the new value.
+	IncrementOffset(
+		ctx context.Context,
+		tx *sql.Tx,
+		app string,
+		count uint64,
+	) (uint64, error)
+
+	// Append appends a single message to an application's stream.
+	Append(
+		ctx context.Context,
+		tx *sql.Tx,
+		app string,
+		offset uint64,
+		typename string,
+		description string,
+		env *envelope.Envelope,
+	) error
+
+	// Get returns the first message at or after a specific offset that matches
+	// a specific filter.
+	//
+	// The driver may leave the source application key and the dogma.Message
+	// fields of the envelope empty, as they are populated by the sql.cursor
+	// implementation.
+	Get(
+		ctx context.Context,
+		db *sql.DB,
+		app string,
+		offset uint64,
+		filterID uint64,
+	) (*persistence.StreamMessage, bool, error)
+}
