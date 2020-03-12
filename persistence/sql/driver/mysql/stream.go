bb02d467d9a08c287a504930cc6c48b014ecd0b6
James Harris <mailjamesharris@gmail.com>
2020-03-12 15:36:19 +1000

    Refactor MySQL stream using `StreamDriver`.

--- persistence/sql/driver/mysql/stream.go
+++ persistence/sql/driver/mysql/stream.go
@@ -3,69 +3,113 @@ package mysql
 import (
 	"context"
 	"database/sql"
-	"reflect"
-	"sync"
 
-	"github.com/dogmatiq/configkit/message"
-	"github.com/dogmatiq/dogma"
 	"github.com/dogmatiq/infix/envelope"
 	"github.com/dogmatiq/infix/internal/x/sqlx"
 	"github.com/dogmatiq/infix/persistence"
 	"github.com/dogmatiq/infix/persistence/sql/internal/streamfilter"
-	"github.com/dogmatiq/linger/backoff"
-	"github.com/dogmatiq/marshalkit"
 )
 
-// Stream is an implementation of persistence.Stream that stores messages
+// StreamDriver is an implementation of driver.StreamDriver that stores messages
 // in a MySQL database.
-type Stream struct {
-	ApplicationKey  string
-	DB              *sql.DB
-	Marshaler       marshalkit.Marshaler
-	BackoffStrategy backoff.Strategy
-}
+type StreamDriver struct{}
 
-// Open returns a cursor used to read messages from this stream.
-//
-// offset is the position of the first message to read. The first message on a
-// stream is always at offset 0.
-//
-// types is a set of message types indicating which message types are returned
-// by Cursor.Next().
-func (s *Stream) Open(
+// FindFilter finds a filter by its hash and type names.
+func (StreamDriver) FindFilter(
 	ctx context.Context,
-	offset uint64,
-	types message.TypeCollection,
-) (persistence.StreamCursor, error) {
-	filterID, err := s.findOrCreateFilter(ctx, types)
-	if err != nil {
-		return nil, err
+	db *sql.DB,
+	hash []byte,
+	names []string,
+) (_ uint64, _ bool, err error) {
+	defer sqlx.Recover(&err)
+
+	tx := sqlx.Begin(ctx, db)
+	defer tx.Rollback()
+
+	filterIDs := sqlx.QueryManyN(
+		ctx,
+		tx,
+		`SELECT
+			id
+		FROM stream_filter
+		WHERE hash = ?
+		FOR UPDATE`,
+		hash,
+	)
+
+	for _, id := range filterIDs {
+		fnames := sqlx.QueryManyS(
+			ctx,
+			tx,
+			`SELECT
+				message_type
+			FROM stream_filter_type
+			WHERE filter_id = ?
+			ORDER BY message_type`,
+			id,
+		)
+
+		if streamfilter.CompareNames(names, fnames) {
+			sqlx.Exec(
+				ctx,
+				tx,
+				`UPDATE stream_filter SET
+					used_at = CURRENT_TIMESTAMP
+				WHERE id = ?`,
+				id,
+			)
+
+			return id, true, tx.Commit()
+		}
 	}
 
-	return &cursor{
-		appKey:    s.ApplicationKey,
-		offset:    offset,
-		filterID:  filterID,
-		db:        s.DB,
-		marshaler: s.Marshaler,
-		counter: backoff.Counter{
-			Strategy: s.BackoffStrategy,
-		},
-		closed: make(chan struct{}),
-	}, nil
+	return 0, false, nil
 }
 
-// Append appends messages to the stream.
-//
-// It returns the next free offset.
-func (s *Stream) Append(
+// CreateFilter creates a filter with the specified hash and type names.
+func (StreamDriver) CreateFilter(
 	ctx context.Context,
-	tx *sql.Tx,
-	envelopes ...*envelope.Envelope,
+	db *sql.DB,
+	hash []byte,
+	names []string,
 ) (_ uint64, err error) {
 	defer sqlx.Recover(&err)
 
-	count := uint64(len(envelopes))
+	tx := sqlx.Begin(ctx, db)
+	defer tx.Rollback()
+
+	id := sqlx.Insert(
+		ctx,
+		tx,
+		`INSERT INTO stream_filter SET
+			hash = ?`,
+		hash,
+	)
+
+	for _, n := range names {
+		sqlx.Exec(
+			ctx,
+			tx,
+			`INSERT INTO stream_filter_type SET
+				filter_id = ?,
+				message_type = ?`,
+			id,
+			n,
+		)
+	}
+
+	return id, tx.Commit()
+}
+
+// IncrementOffset increments an application stream's next offset by the
+// specified amount and returns the new value.
+func (StreamDriver) IncrementOffset(
+	ctx context.Context,
+	tx *sql.Tx,
+	app string,
+	count uint64,
+) (_ uint64, err error) {
+	defer sqlx.Recover(&err)
 
 	sqlx.Exec(
 		ctx,
@@ -83,205 +127,118 @@ func (s *Stream) Append(
 		`SELECT
 			next_offset
 		FROM stream_offset`,
-	) - count
-
-	for _, env := range envelopes {
-		sqlx.Exec(
-			ctx,
-			tx,
-			`INSERT INTO stream SET
-				stream_offset       = ?,
-				message_type        = ?,
-				description         = ?,
-				message_id          = ?,
-				causation_id        = ?,
-				correlation_id      = ?,
-				source_app_name     = ?,
-				source_app_key      = ?,
-				source_handler_name = ?,
-				source_handler_key  = ?,
-				source_instance_id  = ?,
-				created_at          = ?,
-				media_type          = ?,
-				data                = ?`,
-			next,
-			marshalkit.MustMarshalType(
-				s.Marshaler,
-				reflect.TypeOf(env.Message),
-			),
-			dogma.DescribeMessage(env.Message),
-			env.MessageID,
-			env.CausationID,
-			env.CorrelationID,
-			env.Source.Application.Name,
-			env.Source.Application.Key,
-			env.Source.Handler.Name,
-			env.Source.Handler.Key,
-			env.Source.InstanceID,
-			sqlx.MarshalTime(env.CreatedAt),
-			env.Packet.MediaType,
-			env.Packet.Data,
-		)
-
-		next++
-	}
+	)
 
 	return next, nil
 }
 
-// findOrCreateFilter returns the filter ID for a filter that limits cursor
-// results to the given message type names.
-//
-// If no such filter exists, a new one is created.
-func (s *Stream) findOrCreateFilter(
+// Append appends a single message to an application's stream.
+func (StreamDriver) Append(
 	ctx context.Context,
-	types message.TypeCollection,
-) (_ uint64, err error) {
-	defer sqlx.Recover(&err)
-
-	hash, names := streamfilter.Hash(s.Marshaler, types)
-
-	if id, ok := findFilter(ctx, s.DB, hash, names); ok {
-		return id, nil
-	}
-
-	return createFilter(ctx, s.DB, hash, names), nil
-}
-
-// cursor is an implementation of persistence.Cursor that reads messages from a
-// MySQL database.
-type cursor struct {
-	appKey    string
-	offset    uint64
-	filterID  uint64
-	db        *sql.DB
-	marshaler marshalkit.ValueMarshaler
-	counter   backoff.Counter
-	once      sync.Once
-	closed    chan struct{}
-}
-
-// Next returns the next relevant message in the stream.
-//
-// If the end of the stream is reached it blocks until a relevant message is
-// appended to the stream or ctx is canceled.
-func (c *cursor) Next(ctx context.Context) (_ *persistence.StreamMessage, err error) {
-	// Check immediately if the cursor is already closed.
-	select {
-	case <-c.closed:
-		return nil, persistence.ErrStreamCursorClosed
-	default:
-	}
-
-	// Otherwise, setup a context that we can cancel when the cursor is closed.
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	go func() {
-		select {
-		case <-ctx.Done():
-		case <-c.closed:
-			cancel()
-		}
-	}()
-
-	// Finally, if we actually get a context cancelleation error, check if it
-	// was because the cursor was closed, and if so, return a more meaningful.
-	defer func() {
-		if err == context.Canceled {
-			select {
-			case <-c.closed:
-				err = persistence.ErrStreamCursorClosed
-			default:
-			}
-		}
-	}()
-
-	// Convert sqlx panics to errors. This is defer'd LAST so that it recovers
-	// FIRST on the way back out. The error value is used in the defered
-	// functions above.
-	defer sqlx.Recover(&err)
-
-	for {
-		row := c.db.QueryRowContext(
-			ctx,
-			`SELECT
-				stream_offset,
-				message_id,
-				causation_id,
-				correlation_id,
-				source_app_name,
-				source_handler_name,
-				source_handler_key,
-				source_instance_id,
-				created_at,
-				media_type,
-				data
-			FROM stream AS s
-			INNER JOIN stream_filter_type AS t
-			ON t.message_type = s.message_type
-			WHERE s.source_app_key = ?
-			AND s.stream_offset >= ?
-			AND t.filter_id = ?
-			ORDER BY s.stream_offset
-			LIMIT 1`,
-			c.appKey,
-			c.offset,
-			c.filterID,
-		)
-
-		m := persistence.StreamMessage{
-			Envelope: &envelope.Envelope{},
-		}
-
-		var createdAt []byte
-
-		if sqlx.TryScan(
-			row,
-			&m.Offset,
-			&m.Envelope.MessageID,
-			&m.Envelope.CausationID,
-			&m.Envelope.CorrelationID,
-			&m.Envelope.Source.Application.Name,
-			&m.Envelope.Source.Handler.Name,
-			&m.Envelope.Source.Handler.Key,
-			&m.Envelope.Source.InstanceID,
-			&createdAt,
-			&m.Envelope.Packet.MediaType,
-			&m.Envelope.Packet.Data,
-		) {
-			m.Envelope.Source.Application.Key = c.appKey
-			m.Envelope.CreatedAt = sqlx.UnmarshalTime(createdAt)
-			m.Envelope.Message, err = marshalkit.UnmarshalMessage(
-				c.marshaler,
-				m.Envelope.Packet,
-			)
-			if err != nil {
-				return nil, err
-			}
-
-			c.offset = m.Offset + 1
-			c.counter.Reset()
-
-			return &m, nil
-		}
-
-		if err := c.counter.Sleep(ctx, nil); err != nil {
-			return nil, err
-		}
-	}
-}
-
-// Close stops the cursor.
-//
-// Any current or future calls to Next() return a non-nil error.
-func (c *cursor) Close() error {
-	err := persistence.ErrStreamCursorClosed
-
-	c.once.Do(func() {
-		err = nil
-		close(c.closed)
-	})
+	tx *sql.Tx,
+	app string,
+	offset uint64,
+	typename string,
+	description string,
+	env *envelope.Envelope,
+) error {
+	_, err := tx.ExecContext(
+		ctx,
+		`INSERT INTO stream SET
+			stream_offset       = ?,
+			message_type        = ?,
+			description         = ?,
+			message_id          = ?,
+			causation_id        = ?,
+			correlation_id      = ?,
+			source_app_name     = ?,
+			source_app_key      = ?,
+			source_handler_name = ?,
+			source_handler_key  = ?,
+			source_instance_id  = ?,
+			created_at          = ?,
+			media_type          = ?,
+			data                = ?`,
+		offset,
+		typename,
+		description,
+		env.MessageID,
+		env.CausationID,
+		env.CorrelationID,
+		env.Source.Application.Name,
+		env.Source.Application.Key,
+		env.Source.Handler.Name,
+		env.Source.Handler.Key,
+		env.Source.InstanceID,
+		sqlx.MarshalTime(env.CreatedAt),
+		env.Packet.MediaType,
+		env.Packet.Data,
+	)
 
 	return err
+}
+
+// Get returns the first message at or after a specific offset that matches
+// a specific filter.
+func (StreamDriver) Get(
+	ctx context.Context,
+	db *sql.DB,
+	app string,
+	offset uint64,
+	filterID uint64,
+) (_ *persistence.StreamMessage, _ bool, err error) {
+	defer sqlx.Recover(&err)
+
+	row := db.QueryRowContext(
+		ctx,
+		`SELECT
+			stream_offset,
+			message_id,
+			causation_id,
+			correlation_id,
+			source_app_name,
+			source_handler_name,
+			source_handler_key,
+			source_instance_id,
+			created_at,
+			media_type,
+			data
+		FROM stream AS s
+		INNER JOIN stream_filter_type AS t
+		ON t.message_type = s.message_type
+		WHERE s.source_app_key = ?
+		AND s.stream_offset >= ?
+		AND t.filter_id = ?
+		ORDER BY s.stream_offset
+		LIMIT 1`,
+		app,
+		offset,
+		filterID,
+	)
+
+	m := persistence.StreamMessage{
+		Envelope: &envelope.Envelope{},
+	}
+
+	var createdAt []byte
+
+	if sqlx.TryScan(
+		row,
+		&m.Offset,
+		&m.Envelope.MessageID,
+		&m.Envelope.CausationID,
+		&m.Envelope.CorrelationID,
+		&m.Envelope.Source.Application.Name,
+		&m.Envelope.Source.Handler.Name,
+		&m.Envelope.Source.Handler.Key,
+		&m.Envelope.Source.InstanceID,
+		&createdAt,
+		&m.Envelope.Packet.MediaType,
+		&m.Envelope.Packet.Data,
+	) {
+		m.Envelope.CreatedAt = sqlx.UnmarshalTime(createdAt)
+		return &m, true, nil
+	}
+
+	return nil, false, nil
 }
--- persistence/sql/driver/mysql/stream_test.go
+++ persistence/sql/driver/mysql/stream_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/dogmatiq/infix/envelope"
 	"github.com/dogmatiq/infix/internal/x/sqlx"
 	"github.com/dogmatiq/infix/persistence/internal/streamtest"
+	infixsql "github.com/dogmatiq/infix/persistence/sql"
 	. "github.com/dogmatiq/infix/persistence/sql/driver/mysql"
 	"github.com/dogmatiq/infix/persistence/sql/internal/drivertest"
 	"github.com/dogmatiq/linger/backoff"
@@ -29,9 +30,10 @@ var _ = Describe("type Stream (standard test suite)", func() {
 			err = CreateSchema(ctx, db)
 			Expect(err).ShouldNot(HaveOccurred())
 
-			stream := &Stream{
+			stream := &infixsql.Stream{
 				ApplicationKey:  "<app-key>",
 				DB:              db,
+				Driver:          StreamDriver{},
 				Marshaler:       m,
 				BackoffStrategy: backoff.Constant(10 * time.Millisecond),
 			}
