f02e975378fedca40b4d31548694ce49c52aa1bd
James Harris <mailjamesharris@gmail.com>
2020-03-29 13:17:43 +1000

    Setup test suite for the queue subsystem.

---
+++ persistence/internal/providertest/queue.go
@@ -0,0 +1,41 @@
+package providertest
+
+import (
+	"context"
+
+	"github.com/dogmatiq/infix/persistence"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+func declareQueueTests(
+	ctx *context.Context,
+	in *In,
+	out *Out,
+) {
+	ginkgo.Context("package queue", func() {
+		var (
+			provider      persistence.Provider
+			closeProvider func()
+			dataStore     persistence.DataStore
+		)
+
+		ginkgo.BeforeEach(func() {
+			provider, closeProvider = out.NewProvider()
+
+			var err error
+			dataStore, err = provider.Open(*ctx, "<app-key>")
+			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+		})
+
+		ginkgo.AfterEach(func() {
+			if dataStore != nil {
+				dataStore.Close()
+			}
+
+			if closeProvider != nil {
+				closeProvider()
+			}
+		})
+	})
+}
