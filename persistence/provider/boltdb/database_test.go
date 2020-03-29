b7131f462e80b2df3e4b51ab0f3ea3207bba9286
James Harris <mailjamesharris@gmail.com>
2020-03-29 10:01:39 +1000

    Add a test for failure to begin underlying transaction.

---
+++ persistence/provider/boltdb/database_test.go
@@ -0,0 +1,41 @@
+package boltdb_test
+
+import (
+	"context"
+	"time"
+
+	"github.com/dogmatiq/infix/internal/testing/boltdbtest"
+	. "github.com/dogmatiq/infix/persistence/provider/boltdb"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("type database", func() {
+	Describe("func Begin()", func() {
+		It("fails if the underlying database is closed", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+			defer cancel()
+
+			db, close := boltdbtest.Open()
+			defer close()
+
+			provider := &Provider{
+				DB: db,
+			}
+
+			ds, err := provider.Open(ctx, "<app-key>")
+			Expect(err).ShouldNot(HaveOccurred())
+			defer ds.Close()
+
+			close() // Close the underlying BoltDB database.
+
+			tx, err := ds.Begin(ctx)
+			Expect(err).ShouldNot(HaveOccurred())
+			defer tx.Rollback()
+
+			// Perform a write to cause the actual transaction to be started.
+			_, err = tx.SaveEvents(ctx, nil)
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+})
