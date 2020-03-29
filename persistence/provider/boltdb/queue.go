c82030bcf37b465e863388f460a9b0327cc7a03e
James Harris <mailjamesharris@gmail.com>
2020-03-29 14:54:55 +1000

    Implement basic queue features for the BoltDB provider.

--- persistence/provider/boltdb/queue.go
+++ persistence/provider/boltdb/queue.go
@@ -6,14 +6,33 @@ import (
 	"time"
 
 	"github.com/dogmatiq/infix/draftspecs/envelopespec"
+	"github.com/dogmatiq/infix/internal/x/bboltx"
+	"github.com/dogmatiq/infix/persistence/provider/boltdb/internal/pb"
 	"github.com/dogmatiq/infix/persistence/subsystem/queue"
+	"github.com/golang/protobuf/proto"
+	"go.etcd.io/bbolt"
 )
 
 // EnqueueMessages adds messages to the application's message queue.
 func (t *transaction) EnqueueMessages(
 	ctx context.Context,
 	envelopes []*envelopespec.Envelope,
-) error {
+) (err error) {
+	defer bboltx.Recover(&err)
+
+	if err := t.begin(ctx); err != nil {
+		return err
+	}
+
+	messages := bboltx.CreateBucketIfNotExists(
+		t.actual,
+		t.appKey,
+		queueBucketKey,
+		messagesBucketKey,
+	)
+
+	enqueueMessages(messages, envelopes)
+
 	return nil
 }
 
@@ -56,6 +75,72 @@ type queueRepository struct {
 func (r *queueRepository) LoadQueuedMessages(
 	ctx context.Context,
 	n int,
-) ([]*queue.Message, error) {
-	return nil, nil
+) (_ []*queue.Message, err error) {
+	defer bboltx.Recover(&err)
+
+	var result []*queue.Message
+
+	// Execute a read-only transaction.
+	r.db.View(
+		ctx,
+		func(tx *bbolt.Tx) {
+			messages, exists := bboltx.TryBucket(
+				tx,
+				r.appKey,
+				queueBucketKey,
+				messagesBucketKey,
+			)
+
+			if exists {
+				c := messages.Cursor()
+
+				k, v := c.First()
+
+				for k != nil && len(result) < n {
+					m := unmarshalQueueMessage(v)
+					result = append(result, m)
+
+					k, v = c.Next()
+				}
+			}
+		},
+	)
+
+	return result, nil
+}
+
+var (
+	queueBucketKey    = []byte("queue")
+	messagesBucketKey = []byte("messages")
+)
+
+// unmarshalQueueMessage unmarshals a queue message from its protobuf
+// representation.
+func unmarshalQueueMessage(data []byte) *queue.Message {
+	var m pb.QueueMessage
+	bboltx.Must(proto.Unmarshal(data, &m))
+
+	return &queue.Message{
+		Revision: queue.Revision(m.Revision),
+		Envelope: m.Envelope,
+	}
+}
+
+// enqueueMessages writes messages to the queue.
+func enqueueMessages(
+	b *bbolt.Bucket,
+	envelopes []*envelopespec.Envelope,
+) {
+	for _, env := range envelopes {
+		penv := &pb.QueueMessage{
+			Revision: 1,
+			Envelope: env,
+		}
+
+		k := []byte(env.MetaData.MessageId)
+		v, err := proto.Marshal(penv)
+		bboltx.Must(err)
+
+		bboltx.Put(b, k, v)
+	}
 }
