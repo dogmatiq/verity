c90aeae9df169c7368419f7c29cf796818a55c05
James Harris <mailjamesharris@gmail.com>
2020-04-09 09:24:19 +1000

    Add `pipeline.Scope`.

---
+++ fixtures/pipeline.go
@@ -0,0 +1,122 @@
+package fixtures
+
+import (
+	"context"
+	"time"
+
+	"github.com/dogmatiq/infix/envelope"
+	"github.com/dogmatiq/infix/persistence"
+	"github.com/dogmatiq/infix/pipeline"
+)
+
+// SessionStub is a test implementation of the pipeline.Session interface.
+type SessionStub struct {
+	pipeline.Session
+
+	MessageIDFunc    func() string
+	FailureCountFunc func() uint
+	EnvelopeFunc     func() (*envelope.Envelope, error)
+	TxFunc           func(context.Context) (persistence.ManagedTransaction, error)
+	AckFunc          func(context.Context) error
+	NackFunc         func(context.Context, time.Time) error
+	CloseFunc        func() error
+}
+
+// MessageID returns the ID of the message that started the session.
+func (s *SessionStub) MessageID() string {
+	if s.MessageIDFunc != nil {
+		return s.MessageIDFunc()
+	}
+
+	if s.Session != nil {
+		return s.Session.MessageID()
+	}
+
+	return ""
+}
+
+// FailureCount returns the number of times this message has already been
+// attempted, not including this attempt.
+func (s *SessionStub) FailureCount() uint {
+	if s.FailureCountFunc != nil {
+		return s.FailureCountFunc()
+	}
+
+	if s.Session != nil {
+		return s.Session.FailureCount()
+	}
+
+	return 0
+}
+
+// Envelope returns the envelope containing the message to be handled.
+func (s *SessionStub) Envelope() (*envelope.Envelope, error) {
+	if s.EnvelopeFunc != nil {
+		return s.EnvelopeFunc()
+	}
+
+	if s.Session != nil {
+		return s.Session.Envelope()
+	}
+
+	return nil, nil
+}
+
+// Tx returns the transaction used to persist data within this session.
+func (s *SessionStub) Tx(ctx context.Context) (persistence.ManagedTransaction, error) {
+	if s.TxFunc != nil {
+		return s.TxFunc(ctx)
+	}
+
+	if s.Session != nil {
+		return s.Session.Tx(ctx)
+	}
+
+	return nil, nil
+}
+
+// Ack acknowledges successful handling of the message.
+//
+// It commits the changes performed in the session's transaction.
+func (s *SessionStub) Ack(ctx context.Context) error {
+	if s.AckFunc != nil {
+		return s.AckFunc(ctx)
+	}
+
+	if s.Session != nil {
+		return s.Session.Ack(ctx)
+	}
+
+	return nil
+}
+
+// Nack indicates an error while handling the message.
+//
+// It discards the changes performed in the session's transaction and defers
+// handling of the message until n.
+func (s *SessionStub) Nack(ctx context.Context, n time.Time) error {
+	if s.NackFunc != nil {
+		return s.NackFunc(ctx, n)
+	}
+
+	if s.Session != nil {
+		return s.Session.Nack(ctx, n)
+	}
+
+	return nil
+}
+
+// Close releases the session.
+//
+// It must be called regardless of whether Ack() or Nack() are called.
+func (s *SessionStub) Close() error {
+	if s.CloseFunc != nil {
+		return s.CloseFunc()
+	}
+
+	if s.Session != nil {
+		return s.Session.Close()
+	}
+
+	return nil
+}
---
+++ pipeline/pipeline_test.go
@@ -0,0 +1,18 @@
+package pipeline_test
+
+import (
+	"context"
+	"errors"
+
+	. "github.com/dogmatiq/infix/pipeline"
+)
+
+// pass is a Sink that always returns nil.
+func pass(context.Context, *Scope) error {
+	return nil
+}
+
+// fail is a Sink that always returns an error.
+func fail(context.Context, *Scope) error {
+	return errors.New("sink called")
+}
