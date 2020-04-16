4b90ba16cb638a5db4a5aca8a3bc2ba2b4c7d7a2
James Harris <mailjamesharris@gmail.com>
2020-04-17 07:14:20 +1000

    Add `aggregatestore` interfaces.

---
+++ persistence/subsystem/aggregatestore/doc.go
@@ -0,0 +1,3 @@
+// Package aggregatestore defines an API for persisting the aggregate snapshots
+// and enforcing optimistic concurrency control requirements.
+package aggregatestore
---
+++ persistence/subsystem/aggregatestore/revision.go
@@ -0,0 +1,5 @@
+package aggregatestore
+
+// Revision is the version of an aggregate instance, used for optimistic
+// concurrency control.
+type Revision uint64
