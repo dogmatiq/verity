8e7ca4adcba322f95b8dda2ab87d95b19256a0c9
James Harris <mailjamesharris@gmail.com>
2020-05-05 17:36:45 +1000

    Add `Persister` interface, and associated types.

---
+++ persistence/persister.go
@@ -0,0 +1,42 @@
+package persistence
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dogmatiq/infix/persistence/subsystem/eventstore"
+)
+
+// Batch is a set of operations that are committed to the data store atomically
+// using a Persister.
+type Batch []Operation
+
+// BatchResult is the result of a successfully persisted batch of operations.
+type BatchResult struct {
+	// EventStoreItems contains the events from SaveEvent operations.
+	EventStoreItems []*eventstore.Item
+}
+
+// A Persister is an interface for committing batches of atomic operations to
+// the data store.
+type Persister interface {
+	// Persist commits a batch of operations atomically.
+	//
+	// If any one of the operations causes an optimistic concurrency conflict
+	// the entire batch is aborted and a ConflictError is returned.
+	Persist(context.Context, Batch) (BatchResult, error)
+}
+
+// ConflictError is an error indicating one or more operations within a batch
+// caused an optimistic concurrency conflict.
+type ConflictError struct {
+	// Operation is the operation that caused the conflict.
+	Operation Operation
+}
+
+func (e ConflictError) Error() string {
+	return fmt.Sprintf(
+		"optimistic concurrency conflict in %T operation",
+		e.Operation,
+	)
+}
