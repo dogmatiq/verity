507f84fe382cf33c220bd0e3a06f87f2341b7d62
James Harris <mailjamesharris@gmail.com>
2020-05-15 07:58:09 +1000

    Initialize a handler entry point in each app.

--- queue.go
+++ queue.go
@@ -2,9 +2,6 @@ package infix
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/dogmatiq/infix/pipeline"
 )
 
 // runQueueForApp processes messages from the queue.
@@ -12,17 +9,18 @@ func (e *Engine) runQueueForApp(
 	ctx context.Context,
 	a *app,
 ) error {
-	p := &pipeline.QueueSource{
-		Queue:     a.Queue,
-		Pipeline:  a.Pipeline,
-		Semaphore: e.semaphore,
-	}
+	return nil // TODO
+	// p := &pipeline.QueueSource{
+	// 	Queue:     a.Queue,
+	// 	Pipeline:  a.Pipeline,
+	// 	Semaphore: e.semaphore,
+	// }
 
-	err := p.Run(ctx)
+	// err := p.Run(ctx)
 
-	return fmt.Errorf(
-		"stopped consuming from the queue for %s: %w",
-		a.Config.Identity(),
-		err,
-	)
+	// return fmt.Errorf(
+	// 	"stopped consuming from the queue for %s: %w",
+	// 	a.Config.Identity(),
+	// 	err,
+	// )
 }
