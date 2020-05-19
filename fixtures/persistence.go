04aeb46797572de0541d4bf20d931f24f9656434
James Harris <mailjamesharris@gmail.com>
2020-05-19 11:05:04 +1000

    Remove `TransactionStub`.

--- fixtures/persistence.go
+++ fixtures/persistence.go
@@ -3,7 +3,6 @@ package fixtures
 import (
 	"context"
 
-	"github.com/dogmatiq/infix/draftspecs/envelopespec"
 	"github.com/dogmatiq/infix/persistence"
 	"github.com/dogmatiq/infix/persistence/provider/memory"
 	"github.com/dogmatiq/infix/persistence/subsystem/aggregatestore"
@@ -45,7 +44,6 @@ type DataStoreStub struct {
 	EventStoreRepositoryFunc     func() eventstore.Repository
 	QueueStoreRepositoryFunc     func() queuestore.Repository
 	PersistFunc                  func(context.Context, persistence.Batch) (persistence.Result, error)
-	BeginFunc                    func(context.Context) (persistence.Transaction, error)
 	CloseFunc                    func() error
 }
 
@@ -156,18 +154,8 @@ func (ds *DataStoreStub) Persist(ctx context.Context, b persistence.Batch) (pers
 
 // Begin starts a new transaction.
 func (ds *DataStoreStub) Begin(ctx context.Context) (persistence.Transaction, error) {
-	if ds.BeginFunc != nil {
-		return ds.BeginFunc(ctx)
-	}
-
 	if ds.DataStore != nil {
-		tx, err := ds.DataStore.Begin(ctx)
-
-		if tx != nil {
-			tx = &TransactionStub{Transaction: tx}
-		}
-
-		return tx, err
+		return ds.DataStore.Begin(ctx)
 	}
 
 	return nil, nil
@@ -181,120 +169,6 @@ func (ds *DataStoreStub) Close() error {
 
 	if ds.DataStore != nil {
 		return ds.DataStore.Close()
-	}
-
-	return nil
-}
-
-// TransactionStub is a test implementation of the persistence.Transaction
-// interface.
-type TransactionStub struct {
-	persistence.Transaction
-
-	SaveAggregateMetaDataFunc  func(context.Context, *aggregatestore.MetaData) error
-	SaveEventFunc              func(context.Context, *envelopespec.Envelope) (uint64, error)
-	SaveOffsetFunc             func(ctx context.Context, ak string, c, n uint64) error
-	SaveMessageToQueueFunc     func(context.Context, *queuestore.Item) error
-	RemoveMessageFromQueueFunc func(context.Context, *queuestore.Item) error
-
-	CommitFunc   func(context.Context) (persistence.TransactionResult, error)
-	RollbackFunc func() error
-}
-
-// SaveAggregateMetaData persists meta-data about an aggregate instance.
-func (t *TransactionStub) SaveAggregateMetaData(ctx context.Context, md *aggregatestore.MetaData) error {
-	if t.SaveAggregateMetaDataFunc != nil {
-		return t.SaveAggregateMetaDataFunc(ctx, md)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.SaveAggregateMetaData(ctx, md)
-	}
-
-	return nil
-}
-
-// SaveEvent persists an event in the application's event store.
-func (t *TransactionStub) SaveEvent(ctx context.Context, env *envelopespec.Envelope) (uint64, error) {
-	if t.SaveEventFunc != nil {
-		return t.SaveEventFunc(ctx, env)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.SaveEvent(ctx, env)
-	}
-
-	return 0, nil
-}
-
-// SaveOffset persists the "next" offset to be consumed for a specific
-// application.
-func (t *TransactionStub) SaveOffset(
-	ctx context.Context,
-	ak string,
-	c, n uint64,
-) error {
-	if t.SaveOffsetFunc != nil {
-		return t.SaveOffsetFunc(ctx, ak, c, n)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.SaveOffset(ctx, ak, c, n)
-	}
-
-	return nil
-}
-
-// SaveMessageToQueue persists a message to the application's message queue.
-func (t *TransactionStub) SaveMessageToQueue(ctx context.Context, i *queuestore.Item) error {
-	if t.SaveMessageToQueueFunc != nil {
-		return t.SaveMessageToQueueFunc(ctx, i)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.SaveMessageToQueue(ctx, i)
-	}
-
-	return nil
-}
-
-// RemoveMessageFromQueue removes a specific message from the application's
-// message queue.
-func (t *TransactionStub) RemoveMessageFromQueue(ctx context.Context, i *queuestore.Item) error {
-	if t.RemoveMessageFromQueueFunc != nil {
-		return t.RemoveMessageFromQueueFunc(ctx, i)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.RemoveMessageFromQueue(ctx, i)
-	}
-
-	return nil
-}
-
-// Commit applies the changes from the transaction.
-func (t *TransactionStub) Commit(
-	ctx context.Context,
-) (persistence.TransactionResult, error) {
-	if t.CommitFunc != nil {
-		return t.CommitFunc(ctx)
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.Commit(ctx)
-	}
-
-	return persistence.TransactionResult{}, nil
-}
-
-// Rollback aborts the transaction.
-func (t *TransactionStub) Rollback() error {
-	if t.RollbackFunc != nil {
-		return t.RollbackFunc()
-	}
-
-	if t.Transaction != nil {
-		return t.Transaction.Rollback()
 	}
 
 	return nil
