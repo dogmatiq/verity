a083b7601fe727ed07918304a547059350b08d3f
James Harris <mailjamesharris@gmail.com>
2020-05-20 16:33:06 +1000

    Remove `refactor251` package.

--- internal/refactor251/doc.go
+++
@@ -1,5 +0,0 @@
-// Package refactor251 contains temporary tools for refactoring transactions
-// into operation batches.
-//
-// TODO: https://github.com/dogmatiq/infix/issues/251
-package refactor251
