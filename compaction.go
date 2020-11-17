67c20897ff6d312ed21ad332e59f674df799ce2d
James Harris <mailjamesharris@gmail.com>
2020-11-18 09:08:09 +1000

    Run projection compactors when running the engine.

---
+++ compaction.go
@@ -0,0 +1,47 @@
+package verity
+
+import (
+	"context"
+
+	"github.com/dogmatiq/configkit"
+	"github.com/dogmatiq/verity/handler/projection"
+	"github.com/dogmatiq/verity/internal/x/loggingx"
+	"golang.org/x/sync/errgroup"
+)
+
+// runCompactorsForApp runs a projection compactor for a each projection with
+// a specific application.
+func (e *Engine) runCompactorsForApp(ctx context.Context, a *app) error {
+	g, ctx := errgroup.WithContext(ctx)
+
+	for _, h := range a.Config.RichHandlers().Projections() {
+		h := h // capture loop variable
+		g.Go(func() error {
+			return e.runCompactorForProjection(ctx, h, a)
+		})
+	}
+
+	return g.Wait()
+}
+
+// runCompactorForProjection runs a projection compactor for a specific
+// projection.
+func (e *Engine) runCompactorForProjection(
+	ctx context.Context,
+	h configkit.RichProjection,
+	a *app,
+) error {
+	c := &projection.Compactor{
+		Handler:  h.Handler(),
+		Interval: e.opts.ProjectionCompactInterval,
+		Timeout:  e.opts.ProjectionCompactTimeout,
+		Logger: loggingx.WithPrefix(
+			e.logger,
+			"[compactor %s@%s] ",
+			h.Identity().Name,
+			a.Config.Identity().Name,
+		),
+	}
+
+	return c.Run(ctx)
+}
