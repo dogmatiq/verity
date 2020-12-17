a35994fb5de81f684ba55a37a8acdbc4f991d0b0
James Harris <mailjamesharris@gmail.com>
2020-12-17 13:53:21 +1000

    Ping the server before trying the drivers.

--- persistence/sqlpersistence/select.go
+++ persistence/sqlpersistence/select.go
@@ -23,18 +23,25 @@ var builtInDrivers = []Driver{
 func selectDriver(ctx context.Context, db *sql.DB) (Driver, error) {
 	var err error
 
-	for _, d := range builtInDrivers {
-		e := d.IsCompatibleWith(ctx, db)
-		if e == nil {
-			return d, nil
-		}
+	// First, ping the database to see if it's even usable. We'll never be able
+	// to detect what the server is if we can't use it.
+	if e := db.PingContext(ctx); e != nil {
+		err = multierr.Append(err, fmt.Errorf("could not ping the server: %w", e))
+	}
 
-		err = multierr.Append(err, fmt.Errorf(
-			"%T is not compatible with %T: %w",
-			d,
-			db.Driver(),
-			e,
-		))
+	if err == nil {
+		for _, d := range builtInDrivers {
+			e := d.IsCompatibleWith(ctx, db)
+			if e == nil {
+				return d, nil
+			}
+
+			err = multierr.Append(err, fmt.Errorf(
+				"incompatible with %T: %w",
+				d,
+				e,
+			))
+		}
 	}
 
 	return nil, multierr.Append(err, fmt.Errorf(
