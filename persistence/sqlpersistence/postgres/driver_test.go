7119b39e0db8d55022b71f07ef18ab7a4b1d39dc
James Harris <mailjamesharris@gmail.com>
2021-09-13 09:27:39 +1000

    Test PostgreSQL driver with all available SQL drivers.

--- persistence/sqlpersistence/postgres/driver_test.go
+++ persistence/sqlpersistence/postgres/driver_test.go
@@ -20,36 +20,38 @@ var _ = Describe("type driver", func() {
 		db       *sql.DB
 	)
 
-	providertest.Declare(
-		func(ctx context.Context, in providertest.In) providertest.Out {
-			var err error
-			database, err = sqltest.NewDatabase(ctx, sqltest.PGXDriver, sqltest.PostgreSQL)
-			Expect(err).ShouldNot(HaveOccurred())
+	for _, pair := range sqltest.CompatiblePairs(sqltest.PostgreSQL) {
+		providertest.Declare(
+			func(ctx context.Context, in providertest.In) providertest.Out {
+				var err error
+				database, err = sqltest.NewDatabase(ctx, pair.Driver, pair.Product)
+				Expect(err).ShouldNot(HaveOccurred())
 
-			db, err = database.Open()
-			Expect(err).ShouldNot(HaveOccurred())
+				db, err = database.Open()
+				Expect(err).ShouldNot(HaveOccurred())
 
-			err = Driver.CreateSchema(ctx, db)
-			Expect(err).ShouldNot(HaveOccurred())
+				err = Driver.CreateSchema(ctx, db)
+				Expect(err).ShouldNot(HaveOccurred())
 
-			return providertest.Out{
-				NewProvider: func() (persistence.Provider, func()) {
-					return &veritysql.Provider{
-						DB: db,
-					}, nil
-				},
-				IsShared: true,
-			}
-		},
-		func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-			defer cancel()
+				return providertest.Out{
+					NewProvider: func() (persistence.Provider, func()) {
+						return &veritysql.Provider{
+							DB: db,
+						}, nil
+					},
+					IsShared: true,
+				}
+			},
+			func() {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
 
-			err := Driver.DropSchema(ctx, db)
-			Expect(err).ShouldNot(HaveOccurred())
+				err := Driver.DropSchema(ctx, db)
+				Expect(err).ShouldNot(HaveOccurred())
 
-			err = database.Close()
-			Expect(err).ShouldNot(HaveOccurred())
-		},
-	)
+				err = database.Close()
+				Expect(err).ShouldNot(HaveOccurred())
+			},
+		)
+	}
 })
