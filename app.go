aa75f226bf1957614c2ba9addf8355bd963ce6b6
James Harris <contact@jamesharris.id.au>
2024-10-10 16:53:45 +1000

    Fix registeration of command types with command executor.

--- app.go
+++ app.go
@@ -175,7 +175,7 @@ func (e *Engine) newCommandExecutor(
 			Produced: sets.NewFromKeys(
 				cfg.
 					MessageTypes().
-					Consumed(message.CommandKind),
+					Produced(message.CommandKind),
 			),
 		},
 	}
